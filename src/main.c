176b7fcf8ba81cae376193e9fc6b180430ee1643
KneelessCat <steven@birdmail.earth>
2026-02-18 20:36:49 +0000

    Set up project directory, makefile, and notes for next task!

---
+++ src/main.c
@@ -0,0 +1,12 @@
+// Alright.
+//
+// Next task(s) for small start! :D
+//
+// 1. Create a small text window to introduce it, minimal ascii art, with options for loading an image, and exiting
+// 2. Print the rgb values
+// 3. Start with PPM. Really 'easy' starting point. Then worry about other formats
+// 4. We'll want to work with other formats later, will want to check the file format when taking it in (so allow program to accept .png, /jpg etc. and just display a small 'not implemented yet!' message
+
+int main() {
+	return 0;
+}
